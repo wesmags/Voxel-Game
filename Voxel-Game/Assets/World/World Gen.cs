67191f961016f6a6b9895d51191d19a1d0c29a93
wesmags <66344550+wesmags@users.noreply.github.com>
2021-02-23 17:05:53 +1000

    Update World Gen.cs

--- Voxel-Game/Assets/World/World Gen.cs
+++ Voxel-Game/Assets/World/World Gen.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class WorldGen : MonoBehaviour
 {
+
+    // Block List 
     public Transform block00;
 
-
-
     void Start()
-    {
-        
+    {   
+        for ( float xPos = -24; xPos < 24; xPos += 0.25f) 
+        {
+            for ( float zPos = -24; zPos < 24; zPos += 0.25f) 
+            {
+                Instantiate(block00, new Vector3(xPos,0,zPos), block00.rotation);
+            }
+        }
     }
 
     
